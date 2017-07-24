e27dd1b81377f32b529dedde7d4532161d0c2f61
Christian Bjartli <git@c.bjart.li>
2017-07-24 10:54:46 +0200

    Add new parser rules

--- Parser.js
+++ Parser.js
@@ -29,17 +29,17 @@ class Parser {
     }
 
     parseDate() {
-        let astnode;
-        this.tokens.saveCursor();
-        if (
-            (astnode = this.parseDate1()) 
-            || (astnode = this.parseDate2())
-            || (astnode = this.parseDate3())
-            || (astnode = this.parseDate4())
-        ) {
-            return astnode;
-        }
-        return null;
+        let astnode =  
+               this.parseDate1() 
+            || this.parseDate2()
+            || this.parseDate3()
+            || this.parseDate4()
+            || this.parseDate5()
+            || this.parseDate6()
+            || this.parseDate7()
+            || this.parseDate8();
+
+        return astnode;
     }
 
     // <indefdate> <year>
@@ -50,8 +50,7 @@ class Parser {
         if ((child1 = this.parseIndefdate()) 
             && (child2 = this.parseYear())) {
             this.tokens.discardCursor();
-            astnode.children.push(child1);
-            astnode.children.push(child2);
+            astnode.children.push(child1, child2);
             return astnode;
         }
         this.tokens.rewindCursor();
@@ -79,8 +78,7 @@ class Parser {
         this.tokens.saveCursor();
         if ((child1 = this.parseIndefdate()) && (child2 = this.parseIndefdate())) {
             this.tokens.discardCursor();
-            astnode.children.push(child1);
-            astnode.children.push(child2);
+            astnode.children.push(child1, child2);
             return astnode;
         }
         this.tokens.rewindCursor();
@@ -96,21 +94,97 @@ class Parser {
             && (child2 = this.parseDelta()) 
             && (child3 = this.parseDate())) {
             this.tokens.discardCursor();
-            astnode.children.push(child1);
-            astnode.children.push(child2);
-            astnode.children.push(child3);
+            astnode.children.push(child1, child2, child3);
             return astnode;
         }
         this.tokens.rewindCursor();
         return null;
     }
 
-    parseIndefdate() {
-        let astnode;
-        if (astnode = this.parseIndefdate1()) {
+    // <nameddate>
+    parseDate5() {
+        let astnode = {type: 'date', prod: 5, children: []};
+        let child;
+        this.tokens.saveCursor();
+        if (child = this.parseNameddate()) {
+            this.tokens.discardCursor();
+            astnode.children.push(child);
             return astnode;
         }
+        this.tokens.rewindCursor();
         return null;
+    }
+
+    // <namedindefdate> <year>
+    parseDate6() {
+        let astnode = {type: 'date', prod: 6, children: []};
+        this.tokens.saveCursor();
+
+        let child1, child2;
+        let parse = 
+               (child1 = this.parseNamedindefdate())
+            && (child2 = this.parseYear())
+            
+        if (parse) {
+            this.tokens.discardCursor();
+            astnode.children.push(child1, child2);
+            return astnode;
+        }
+
+        this.tokens.rewindCursor();
+        return null;
+    }
+
+    // <namedindefdate>
+    parseDate7() {
+        let astnode = {type: 'date', prod: 7, children: []};
+        this.tokens.saveCursor();
+
+        let child;
+        let parse = (child = this.parseNamedindefdate())
+            
+        if (parse) {
+            this.tokens.discardCursor();
+            astnode.children.push(child);
+            return astnode;
+        }
+
+        this.tokens.rewindCursor();
+        return null;
+    }
+
+    parseIndefdate() {
+        let astnode = 
+            this.parseIndefdate1();
+
+        return astnode;
+    }
+    
+    // <namedindefdate> <year>
+    parseDate7() {
+        let astnode = {type: 'date', prod: 8, children: []};
+        this.tokens.saveCursor();
+
+        let child1, child2;
+        let parse = 
+               (child1 = this.parseNumdaymod())
+            && (child2 = this.parseYear())
+            
+        if (parse) {
+            this.tokens.discardCursor();
+            astnode.children.push(child1, child2);
+            return astnode;
+        }
+
+        this.tokens.rewindCursor();
+        return null;
+    }
+
+    parseIndefdate() {
+        let astnode = 
+            this.parseIndefdate1();
+
+        return astnode;
     }
 
     // <month> <daynum>
@@ -121,8 +195,7 @@ class Parser {
         if ((child1 = this.parseMonth())
             && (child2 = this.parseDaynum())) {
             this.tokens.discardCursor();
-            astnode.children.push(child1);
-            astnode.children.push(child2);
+            astnode.children.push(child1, child2);
             return astnode;
         }
         this.tokens.rewindCursor();
@@ -135,19 +208,7 @@ class Parser {
         this.tokens.saveCursor();
         let token = this.tokens.next();
 
-        if (token.type == 'kw' && 
-            (token.val == 'january'
-            || token.val == 'february'
-            || token.val == 'march'
-            || token.val == 'april' 
-            || token.val == 'may'
-            || token.val == 'june'
-            || token.val == 'july'
-            || token.val == 'august'
-            || token.val == 'september'
-            || token.val == 'october'
-            || token.val == 'november'
-            || token.val == 'december')) {
+        if (token.type == 'month') {
             this.tokens.discardCursor();
             astnode.children.push(token);
             return astnode;
@@ -180,13 +241,12 @@ class Parser {
     }
 
     parseYear() {
-        let astnode;
-        if ((astnode = this.parseYear1()) 
-            || (astnode = this.parseYear2()) 
-            || (astnode = this.parseYear3())) {
-            return astnode;
-        }
-        return null;
+        let astnode = 
+               this.parseYear1() 
+            || this.parseYear2()
+            || this.parseYear3();
+
+        return astnode;
     }
 
     // <int> 'bc' || <int> 'ac' || ...
@@ -199,8 +259,7 @@ class Parser {
         if (token1.type == 'int' && token2.type == 'kw' && 
             (token2.val == 'bc' || token2.val == 'ad' || token2.val == 'bce' || token2.val == 'ce')) {
             this.tokens.discardCursor();
-            astnode.children.push(token1);
-            astnode.children.push(token2);
+            astnode.children.push(token1, token2);
             return astnode;
         }
 
@@ -271,8 +330,7 @@ class Parser {
             && (token1.val == 'day' || token1.val == 'days')
             && (token2.val == 'before' || token2.val == 'after')) {
             this.tokens.discardCursor();
-            astnode.children.push(token1);
-            astnode.children.push(token2);
+            astnode.children.push(token1, token2);
             return astnode;
         }
 
@@ -286,6 +344,57 @@ class Parser {
         if (token.type == 'int') {
             this.tokens.discardCursor();
             return token;
+        }
+        this.tokens.rewindCursor();
+        return null;
+    }
+
+    parseNameddate() {
+        this.tokens.saveCursor();
+        let token = this.tokens.next();
+        if (token.type == 'nameddate') {
+            this.tokens.discardCursor();
+            return token;
+        }
+        this.tokens.rewindCursor();
+        return null;
+    }
+
+    parseNamedindefdate() {
+        this.tokens.saveCursor();
+        let token = this.tokens.next();
+        if (token.type == 'namedindefdate') {
+            this.tokens.discardCursor();
+            return token;
+        }
+        this.tokens.rewindCursor();
+        return null;
+    }
+
+    parseNumdaymod() {
+        let astnode = {type: 'numdaymod', children: []};
+        this.tokens.saveCursor();
+        let token1 = this.tokens.peek();
+        
+        if (token1.type == 'kw' && token1.val == 'the') {
+            token1 = this.tokens.next();
+        } else {
+            token1 = { type: 'empty', val: null }
+        }
+
+        let token2 = this.tokens.next();
+        let token3 = this.tokens.next();
+        let token4 = this.tokens.next();
+
+        let parse = 
+               (token2.type == 'ord')
+            && (token3.type == 'kw' && token3.val == 'day')
+            && (token4.type == 'kw' && token4.val == 'of')
+
+        if (parse) {
+            this.tokens.discardCursor();
+            astnode.children.push(token1, token2, token3, token4);
+            return astnode;
         }
         this.tokens.rewindCursor();
         return null;
